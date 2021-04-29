9a43065f6f33918c7dac38aff0561280333a1f52
Marvellous Olobio <69377660+Castor0@users.noreply.github.com>
2021-04-29 16:44:02 +0000

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if(this.scrollY > 20) 
+      $(".navbar").addClass("sticky");
+    else
+      $(".navbar").removeClass("sticky");
+  });
+
+  $('.menu-toggler').click(function(){
+    $(this).toggleClass("active");
+    $(".navbar-menu").toggleClass("active");
+  });
+
+  $(".works").magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery:{enabled:true}
+  });
+});
+
